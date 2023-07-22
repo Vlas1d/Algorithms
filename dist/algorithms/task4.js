d49edfe2b17a6fd854e85082ca345312c9e832f8
Vlas1d <vlasid753@gmail.com>
2023-07-23 02:33:09 +0300

    task4 update1

--- dist/algorithms/task4.js
+++ dist/algorithms/task4.js
@@ -1,1 +1,12 @@
+const input_ = document.getElementById('input');
+const button_ = document.getElementById('button');
+let equation;
+button_.addEventListener('click', () => {
+    equation = input_.value.split('');
+    checkEquation();
+});
+function checkEquation() {
+    for (let i = 0; i < equation.length; i++) {
+    }
+}
 //# sourceMappingURL=task4.js.map
